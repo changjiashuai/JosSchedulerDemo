a60c6647593fd1c6ceced17f9808010a2fd053f6
changjiashuai <changjiashuai@gmail.com>
2016-09-20 18:42:55 +0800

    finish -> next Trigger

--- app/src/main/java/io/github/changjiashuai/josschedulerdemo/MainActivity.java
+++ app/src/main/java/io/github/changjiashuai/josschedulerdemo/MainActivity.java
@@ -1,13 +1,25 @@
 package io.github.changjiashuai.josschedulerdemo;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+  private JobScheduler mJobScheduler;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    mJobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+    JobInfo.Builder builder = new JobInfo.Builder(1, new ComponentName(getPackageName(), JobSchedulerService.class.getName()));
+    builder.setPeriodic(3000);
+    if (mJobScheduler.schedule(builder.build()) <= 0) {
+      //something goes wrong
+    }
   }
 }
